d9a85b0dd12bca9642f0a6e7e6a9c985a24bc1de
maliVishal01 <123369587+maliVishal01@users.noreply.github.com>
2026-02-06 19:09:30 +0530

    Update settings.py

--- gym_project/settings.py
+++ gym_project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ib7jq+u@3&w$flar34(yx%u*lucwj_m--n*6a$^fo(_fh^e&vi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 STATIC_URL = '/static/'
